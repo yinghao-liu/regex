c990abb232e0b8b5e62bc044630caef59d0eb9a0
yinghao-liu <yinghao_liu@163.com>
2017-05-17 18:05:29 +0800

    a test in practice

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include "cregex.h"
 int main(void)
 {
-	char result[20]={0};
-	if (NULL != cregex("ab(.*)", "absdkjsd", result)){
+	char result[200]={0};
+	if (0 == cregex("\"uid\":\"([0-9]*)\"", "\"uid\":\"30710830\",\"albumUrl\":\"http://chunan.19lou.com/user/album?uid=30710830&page=1\",", result)){
+
 		printf("here is main %s\n", result);
 	}
 	return 0;
